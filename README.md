0eba3366b770f8ff42a5fe830a55c780400bbc40
Will <will.liu@nakedhub.cn>
2016-05-19 11:29:46 +0800

    package

---
+++ README.md
@@ -0,0 +1,11 @@
+# copy file loader for webpack
+
+## Usage
+
+```javascript
+{test: /\.(eot|ttf)$/, loader: 'copy-file-loader?copyto=[path][name].[ext]&context=./src/'}
+```
+
+## License
+
+MIT (http://www.opensource.org/licenses/mit-license.php)
---
+++ index.js
@@ -0,0 +1,54 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author liuzhenwei
+*/
+var loaderUtils = require("loader-utils");
+var mime = require("mime");
+
+module.exports = function(content) {
+
+	this.cacheable && this.cacheable();
+
+	if(!this.emitFile) throw new Error("emitFile is required from module system");
+
+	var query = loaderUtils.parseQuery(this.query);
+
+	var limit = (this.options && this.options.url && this.options.url.dataUrlLimit) || 1;
+	if(query.limit) {
+		limit = parseInt(query.limit, 10);
+	}
+
+	var mimetype = query.mimetype || query.minetype || mime.lookup(this.resourcePath);
+
+	if(limit <= 0 || content.length < limit) {
+		
+		return "module.exports = " + JSON.stringify("data:" + (mimetype ? mimetype + ";" : "") + "base64," + content.toString("base64"));
+	
+	} else {
+
+		query.copyto = query.copyto || "[hash].[ext]";
+		query.name = query.name || query.copyto;
+
+		var url = loaderUtils.interpolateName(this, query.copyto, {
+			context: query.context || this.options.context,
+			content: content,
+			regExp: query.regExp
+		});
+
+		var name = loaderUtils.interpolateName(this, query.name, {
+			context: query.context || this.options.context,
+			content: content,
+			regExp: query.regExp
+		});
+
+		var rawRequest = this._module.rawRequest;
+
+		if( rawRequest.indexOf(url) > -1 ){
+			name = rawRequest.replace(url, name); 
+		}
+
+		this.emitFile(url, content);
+		return "module.exports = __webpack_public_path__ + " + JSON.stringify(name) + ";";
+	}
+};
+module.exports.raw = true;
